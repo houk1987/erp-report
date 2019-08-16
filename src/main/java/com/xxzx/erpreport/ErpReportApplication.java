c07d18c29b44e08d5aee48e629b8dbcadc766bbd
houk1987 <598470593@qq.com>
2019-08-16 11:12:53 +0800

    erp-report init

---
+++ src/main/java/com/xxzx/erpreport/ErpReportApplication.java
@@ -0,0 +1,13 @@
+package com.xxzx.erpreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpReportApplication.class, args);
+	}
+
+}
